3d3ed50e7c04524eb01b4900229bdec1538c819e
Ricardo <liedsondias1957@gmail.com>
2026-05-15 12:35:01 +0200

    Configure R2 upload endpoint

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = 'https://lauzvzwhfoqmjhzmigxr.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhdXp2endoZm9xbWpoem1pZ3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MDM4OTQsImV4cCI6MjA5MzA3OTg5NH0.w8UP1FPxE0PFxthn0P_kh0q-TMqHYI8ObrJEh7Koixw';
 export const SUPABASE_ADMIN_EMAILS = ['admin@horizon.pt'];
-export const R2_UPLOAD_ENDPOINT = '';
+export const R2_UPLOAD_ENDPOINT = 'https://upload.horizonaudios.com';
