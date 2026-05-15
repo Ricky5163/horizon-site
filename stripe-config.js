e96edf38294def22126d1198b3bb512db887d04b
Ricardo <liedsondias1957@gmail.com>
2026-05-15 14:23:08 +0200

    Add Stripe payment links

--- stripe-config.js
+++ stripe-config.js
@@ -1,6 +1,6 @@
 export const STRIPE_PAYMENT_LINKS = {
-  plus: '',
-  annual: '',
+  plus: 'https://buy.stripe.com/aFa5kE1CG0mI7xggcYfbq00',
+  annual: 'https://buy.stripe.com/00w8wQ4OS6L67xgbWIfbq01',
 };
 
 export const STRIPE_CUSTOMER_PORTAL_URL = '';
