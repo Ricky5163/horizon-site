2f76511714e8e3c161df2c2d50c70702c07ebafe
Ricardo <liedsondias1957@gmail.com>
2026-05-15 16:08:18 +0200

    Add Stripe test payment links

--- stripe-config.js
+++ stripe-config.js
@@ -4,8 +4,8 @@ export const STRIPE_PAYMENT_LINKS = {
     annual: 'https://buy.stripe.com/00w8wQ4OS6L67xgbWIfbq01',
   },
   test: {
-    plus: '',
-    annual: '',
+    plus: 'https://buy.stripe.com/test_aFa5kE1CG0mI7xggcYfbq00',
+    annual: 'https://buy.stripe.com/test_00w8wQ4OS6L67xgbWIfbq01',
   },
 };
 
